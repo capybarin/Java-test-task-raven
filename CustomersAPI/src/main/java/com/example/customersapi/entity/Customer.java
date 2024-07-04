4276e22ff303b7f0e4df6d98e4ed4fcd5f4973e3
capybarin <vladibzd@gmail.com>
2024-07-04 19:17:08 +0300

    Created entity for customers

---
+++ CustomersAPI/src/main/java/com/example/customersapi/entity/Customer.java
@@ -0,0 +1,52 @@
+package com.example.customersapi.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Entity
+@NoArgsConstructor
+@Table(name = "customers")
+public class Customer {
+
+    @Id
+    @Getter
+    @Setter
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Getter
+    @Setter
+    @Column(name = "created")
+    private Integer created;
+
+    @Getter
+    @Setter
+    @Column(name = "updated")
+    private Integer updated;
+
+    @Getter
+    @Setter
+    @Column(name = "full_name")
+    private String fullName;
+
+    @Getter
+    @Setter
+    @Column(name = "email")
+    private String email;
+
+    @Getter
+    @Setter
+    @Column(name = "phone")
+    private String phone;
+
+    @Getter
+    @Setter
+    @Column(name = "is_active")
+    private Boolean isActive;
+}
